28cc00033d890abb20a2ea4a3b56446876ad4291
damia <s25866@pjwstk.edu.pl>
2023-04-09 22:12:39 +0200

    NOW you can start game and exit from game, i hate git....

---
+++ src/main/java/com/example/leviatan/Utilz/WindowStates.java
@@ -0,0 +1,8 @@
+package com.example.leviatan.Utilz;
+
+public enum WindowStates {
+    MENU,//wiadomo
+    CREATE_NEW_GAME,//tworzenie gry
+    GAME;//inne miejsca
+    public static WindowStates status = GAME;
+}
